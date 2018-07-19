411213d0ff54e07bda3dfecf4a261272e76891a7
yan17asn <248764378@qq.com>
2018-07-18 22:54:44 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WEB120 Portal Page</title>
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<meta charset="utf-8">
+<script src="https://use.fontawesome.com/6a71565c22.js"></script>
+<link rel="stylesheet" href="css/nav.css" />
+<link rel="stylesheet" href="css/portal.css" />
+<link rel="stylesheet" href="css/form.css" />
+</head>
+
+<body>
+<!-- START WRAPPER -->
+<div class="wrapper">
+<header>
+  <h1><a href="index.php"><i class="logo fa fa-home"></i>Weiyan's SCC WEB120 Portal</a></h1>
+  <nav>
+    <ul class="topnav" id="myTopnav">
+      <li><a href="index.php" class="selected">Welcome</a></li>
+      <li><a href="big/index.php">Big</a></li>
+      <li><a href="aia.php">AIA</a></li>
+      <li><a href="flowchart.php">Flowchart</a></li>
+      <li><a href="fp/index.php">Final Project</a></li>
+      <li><a href="contact.php">Contact Weiyan</a></li>
+      <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
+    </ul>
+  </nav>
+</header>
+        
+<h2>Contact weiyan</h2>
+<?php include "includes/simple.php";?>
+<p class="clear-recaptcha"></p>
+ 
+<!-- START Footer -->
+<footer><small>&copy; <?php echo date("Y");?>, All Rights Reserved, <a href="http://validator.w3.org/check?uri=referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a></small></footer>
+
+<!-- END Footer --> 
+
+    <!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
+    <script>
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+     </script>
+</div>
+<!-- END WRAPPER -->
+
+</body>
+</html>
